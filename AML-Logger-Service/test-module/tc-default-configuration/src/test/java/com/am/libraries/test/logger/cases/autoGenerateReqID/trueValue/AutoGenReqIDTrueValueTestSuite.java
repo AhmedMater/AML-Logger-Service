f5b971b734cd07d3a1a4e10bce0cc4f31d9dc5e1
Ahmed Mater <ahmedmotair@gmail.com>
2020-12-09 22:02:20 +0200

    Remove Unneeded Imports

--- AML-Logger-Service/test-module/tc-default-configuration/src/test/java/com/am/libraries/test/logger/cases/autoGenerateReqID/trueValue/AutoGenReqIDTrueValueTestSuite.java
+++ AML-Logger-Service/test-module/tc-default-configuration/src/test/java/com/am/libraries/test/logger/cases/autoGenerateReqID/trueValue/AutoGenReqIDTrueValueTestSuite.java
@@ -3,13 +3,6 @@ package com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.GenerateAppSessionAspectTestCases;
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.LogArgumentTestCases;
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.RESTHeadersInLogPrefixTestCases;
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.LogInfoTestCases;
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.LogErrorTestCases;
-import com.am.libraries.test.logger.cases.autoGenerateReqID.trueValue.LogFnInputOutputAspectTestCases;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ GenerateAppSessionAspectTestCases.class, LogArgumentTestCases.class,
         LogInfoTestCases.class, LogErrorTestCases.class, LogFnInputOutputAspectTestCases.class,
--- AML-Logger-Service/test-module/tc-default-configuration/src/test/java/com/am/libraries/test/logger/cases/defaultConfiguration/DefaultConfigurationTestSuite.java
+++ AML-Logger-Service/test-module/tc-default-configuration/src/test/java/com/am/libraries/test/logger/cases/defaultConfiguration/DefaultConfigurationTestSuite.java
@@ -1,11 +1,5 @@
 package com.am.libraries.test.logger.cases.defaultConfiguration;
 
-import com.am.libraries.test.logger.cases.defaultConfiguration.GenerateAppSessionAspectTestCases;
-import com.am.libraries.test.logger.cases.defaultConfiguration.LogArgumentTestCases;
-import com.am.libraries.test.logger.cases.defaultConfiguration.RESTHeadersInLogPrefixTestCases;
-import com.am.libraries.test.logger.cases.defaultConfiguration.LogInfoTestCases;
-import com.am.libraries.test.logger.cases.defaultConfiguration.LogErrorTestCases;
-import com.am.libraries.test.logger.cases.defaultConfiguration.LogFnInputOutputAspectTestCases;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
